4d0b65eed9b5e78ea1c51ca8bff807367e298063
jintang22 <jintang22@gmail.com>
2023-03-01 20:51:52 +0900

    dd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+class testTang {
+    int a = 100;
+}
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        System.out.println(        new testTang().a );
+    }
+}
